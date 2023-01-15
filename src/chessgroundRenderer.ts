e72f988834a4646ad3984c7c1e593e29df255647
Ely Ronnen <elyronnen@gmail.com>
2023-01-16 00:41:59 +0200

    parse arrows, squares and lastMove

--- src/chessgroundRenderer.ts
+++ src/chessgroundRenderer.ts
@@ -1,14 +1,41 @@
 import { Chessground } from "chessground";
+import { colors} from "chessground/types";
 import type { Config } from "chessground/config";
-import {Color, colors} from 'chessground/types';
+import type {Color, Key} from 'chessground/types';
+import type { DrawShape } from "chessground/draw";
 
 import "chessground/assets/chessground.base.css";
 import "chessground/assets/chessground.brown.css";
 import "chessground/assets/chessground.cburnett.css";
 
 const chessgroundClass = 'chessground-markdown';
+const configDefaultArrowColor = 'green';
+const configDefaultSquareColor = 'blue';
 
-function parseChessBlock(element: HTMLElement, config: Config) {
+function parseSquares(line: string) : Key[] {
+  const result : Key[] = [];
+
+  let currentFile : string | null = null;
+  for (const char of line) {
+    if (currentFile === null) {
+      if (char.toLowerCase() >= 'a' && char.toLowerCase() <= 'h') {
+        currentFile = char.toLowerCase();
+      }
+    }
+    else {
+      if (char >= '1' && char <= '8') {
+        result.push((currentFile + char) as Key);
+        currentFile = null;
+      }
+    }
+  }
+
+  return result;
+}
+
+function parseChessBlock(element: HTMLElement, config: Config) : DrawShape[] {
+  const shapes : DrawShape[] = [];
+
   // I think yaml library here is an overkill
   for (const line of element.innerHTML.split('\n')) {
     const delimeterPosition = line.indexOf(':');
@@ -20,7 +47,7 @@ function parseChessBlock(element: HTMLElement, config: Config) {
     const option = line.substring(0, delimeterPosition);
     const value = line.substring(delimeterPosition + 1).trim();
 
-    switch (option.toLocaleLowerCase()) {
+    switch (option.toLowerCase()) {
       case "fen":
         config.fen = value;
         break;
@@ -29,14 +56,50 @@ function parseChessBlock(element: HTMLElement, config: Config) {
           config.orientation = value as Color;
         }
         break;
+      case "lastmove": {
+        const lastMoveSquares = parseSquares(value);
+        if (lastMoveSquares.length >= 2) {
+          config.lastMove = lastMoveSquares.slice(0, 2);
+        }
+        break;
+      }
+      case "arrows": {
+        config.drawable!.enabled = true;
+        const arrowSquares = parseSquares(value);
+        for (let i = 0; i < arrowSquares.length - 1; i += 2) {
+          shapes.push({
+            orig: arrowSquares[i],
+            dest: arrowSquares[i+1],
+            brush: configDefaultArrowColor
+          });
+        }
+        break;
+      }
+      case "squares": {
+        config.drawable!.enabled = true;
+        const squares = parseSquares(value);
+        for (const square of squares) {
+          shapes.push({
+            orig: square,
+            brush: configDefaultSquareColor
+          });
+        }
+      }
       case "moveable":
-        if (value.toLocaleLowerCase() === "true") {
+        if (value.toLowerCase() === "true") {
           config.draggable!.enabled = true;
           config.selectable!.enabled = true;
         }
         break;
+      case "drawable":
+        if (value.toLowerCase() === "true") {
+          config.drawable!.enabled = true;
+        }
+        break;
     }
   }
+
+  return shapes;
 }
 
 function renderChessgroundBlock(element: HTMLElement) {
@@ -50,11 +113,23 @@ function renderChessgroundBlock(element: HTMLElement) {
     },
     selectable: {
       enabled: false
+    },
+    drawable: {
+      enabled: false,
+      shapes: []
     }
   };
   
-  parseChessBlock(element, config);
-  Chessground(element, config);
+  const shapes = parseChessBlock(element, config);
+  const boardApi = Chessground(element, config);
+
+  if (shapes.length > 0) {
+    boardApi.setShapes(shapes);
+  }
+
+  if (!config.drawable!.enabled && !config.selectable!.enabled && !config.draggable!.enabled) {
+    boardApi.state.dom.elements.board.style.cursor = "default";
+  }
 }
 
 export function renderAllChessBlocksInElement(root: HTMLElement) {
