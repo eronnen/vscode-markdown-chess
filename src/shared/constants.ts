8f3a8e807021d9dff5fae51b9d4d0a051b7ddd31
Ely Ronnen <elyronnen@gmail.com>
2023-02-04 00:44:30 +0200

    adding lichess font for game control buttons

--- src/shared/constants.ts
+++ src/shared/constants.ts
@@ -1,6 +1,7 @@
 export const CHESSGROUND_CONTAINER_CLASS = "chessgroundMarkdownContainer";
 export const CHESSGROUND_CLASS = "chessgroundMarkdown";
 export const CHESSGROUND_INFO_CLASS = "chessgroundInfo";
+export const CHESSGROUND_MOVES_CLASS = "chessgroundMoves";
 export const DEFAULT_BOARD_GEOMETRY = "is2d";
 export const DEFAULT_PIECE_SET: PieceSet = "merida";
 export const DEFAULT_ARROW_COLOR = "green";
